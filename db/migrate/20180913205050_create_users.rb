5f313e145dd4e93f0ea00683ecae262e5a2f7445
Ibteda Sabur <akkount35@gmail.com>
2018-09-13 17:53:25 -0400

    added age column to users

--- db/migrate/20180913205050_create_users.rb
+++ db/migrate/20180913205050_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.integer :age
       t.integer :phone_number
       t.string :email
       t.string :password_digest
