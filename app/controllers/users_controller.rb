6c3df268511bd77595750abe9a20f4b0f3e10a9c
Ibteda Sabur <akkount35@gmail.com>
2018-09-14 11:37:09 -0400

    added bcrypt to gems and made a create action for user#new http post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      p @user #for debugging
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
@@ -10,5 +20,11 @@ class UsersController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:age,:phone_number,:email,:password,:password_confirmation,:interests)
   end
 end
