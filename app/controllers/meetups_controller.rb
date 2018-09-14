cce06c1fd1b50ead2ec740067db0d515522896ea
Bemilton Suero <bemsuero@yahoo.com>
2018-09-14 13:58:09 -0400

    working on directions, next going to work on link between users

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -9,16 +9,26 @@ class MeetupsController < ApplicationController
     @meetup = Meetup.new
     location = params[:location]
     uri = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7079996,-74.0067468&rankby=distance&type=restaurant&key=#{ENV['API_TOKEN']}")
-    raw_data = HTTParty.get(url)
-    # if @raw_data.is_a?(Net::HTTPSuccess)
+    raw_data = HTTParty.get(uri)
     json = raw_data.parsed_response
-    @moreresults = json["next_page_token"]
     @places = json["results"]
-    # else
   end
-  # end
 
+  def show
 
+  end
+
+  def directions
+    house = params[:house_number]
+    address = params[:address]
+    city = params[:city]
+    state = params[:state]
+    to_house = params[:to_house_number]
+    to_address = params[:to_address]
+    to_city = params[:to_city]
+    to_state = params[:to_state]
+    redirect_to "https://www.google.com/maps/dir/?api=1&origin=#{house}+#{address}+#{city}+#{state}&destination=#{to_house}+#{to_address}+#{to_city}+#{to_state}&travelmode=walking"
+  end
 
   def edit
   end
@@ -26,6 +36,4 @@ class MeetupsController < ApplicationController
   def index
   end
 
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ root to: "users#new"
   resources :meetups
   resources :users
 
+  get "directions" => "meetups#directions"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
