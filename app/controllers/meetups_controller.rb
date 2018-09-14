f06cc1968006902a7a71f9675b84dd193695fd20
Bemilton Suero <bemsuero@yahoo.com>
2018-09-14 09:45:36 -0400

    working with mapping

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,6 +1,23 @@
 class MeetupsController < ApplicationController
+require 'net/http'
+require 'uri'
+require 'json'
+
+
   def new
+    @meetup = Meetup.new
+    location = params[:location]
+    # uri = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7079996,-74.0067468&rankby=distance&type=restaurant&key=#{ENV['API_TOKEN']}") what we'll actually be using.
+    uri = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7079996,-74.0067468&rankby=distance&type=restaurant&key=AIzaSyCk6eRqZiFhkKrymT8EUR21OZ4Jf2J9Xgs")
+    raw_data = Net::HTTP.get(uri)
+    # if @raw_data.is_a?(Net::HTTPSuccess)
+    @places = JSON.parse(raw_data)
+
+    # else
   end
+  # end
+
+
 
   def edit
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'meetups/new'
-  get 'meetups/edit'
-  get 'meetups/index'
-  get 'meetups/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/index'
+root to: "users#new"
+
+  resources :meetups
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
