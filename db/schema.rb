a84f2db657fecde231989826deedc69a693698a2
jfrazier <johnfrazier1222@gmail.com>
2018-09-17 12:01:11 -0400

    resposivness and form popups

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
+rail# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -9,7 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 
 ActiveRecord::Schema.define(version: 2018_09_14_202736) do
 
@@ -26,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_09_14_202736) do
   end
 
   create_table "meetup_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "meetup_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -42,8 +43,6 @@ ActiveRecord::Schema.define(version: 2018_09_14_202736) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-
-    t.integer "age"
     t.integer "phone_number"
     t.string "email"
     t.string "password_digest"
@@ -51,7 +50,6 @@ ActiveRecord::Schema.define(version: 2018_09_14_202736) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
   end
 
 end
