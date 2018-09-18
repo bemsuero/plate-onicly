a3972c26cec877f111d0025d7c9716f87c1eb837
Ibteda Sabur <akkount35@gmail.com>
2018-09-18 15:29:43 -0400

    moved relevant code from applcation mailer to guest user mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'plateonicly@gmail.com'
   layout 'mailer'
-
-  def confirmation_email(guest_user)
-    @guest_user = guest_user
-    mail(to: @guest_user[:email], subject: 'You have succesfully joined a meetup')
-  end
 end
--- app/mailers/guest_user_mailer.rb
+++ app/mailers/guest_user_mailer.rb
@@ -1,2 +1,6 @@
 class GuestUserMailer < ApplicationMailer
+  def confirmation_email(guest_user)
+    @guest_user = guest_user
+    mail(to: @guest_user[:email], subject: 'You have succesfully joined a meetup')
+  end
 end
