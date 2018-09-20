4d3f0e5b410ebd43a6615415d7e3d99972c3116d
Bemilton Suero <bemsuero@yahoo.com>
2018-09-20 14:00:01 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# config/application.rb
-config.assets.initialize_on_precompile = false
-
-
 module Project7
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
